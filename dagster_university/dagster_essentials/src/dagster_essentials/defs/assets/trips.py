835648831aefdc5278f3b36872b72d27c4f52ec1
33Elwood <petethielen@hotmail.com>
2026-09-22 18:34:54 +0000

    modified:   dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -1,6 +1,6 @@
 # src/dagster_essentials/defs/assets/trips.py
 
-import duckdb
+from dagster_duckdb import DuckDBResource
 import os
 import dagster as dg
 from dagster._utils.backoff import backoff
@@ -36,16 +36,15 @@ def taxi_zones_file() -> None:
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
 
-# src/dagster_essentials/defs/assets/trips.py
+
+
+
 @dg.asset(
-    deps=["taxi_trips_file"]
+    deps=["taxi_trips_file"],
 )
-def taxi_trips() -> None:
-    """
-      The raw taxi trips dataset, loaded into a DuckDB database
-    """
+def taxi_trips(database: DuckDBResource) -> None:
     query = """
-        create or replace table trips as (
+        create or replace table taxi_trips as (
           select
             VendorID as vendor_id,
             PULocationID as pickup_zone_id,
@@ -61,15 +60,10 @@ def taxi_trips() -> None:
         );
     """
 
-    conn = backoff(
-        fn=duckdb.connect,
-        retry_on=(RuntimeError, duckdb.IOException),
-        kwargs={
-            "database": os.getenv("DUCKDB_DATABASE"),
-        },
-        max_retries=10,
-    )
-    conn.execute(query)
+    with database.get_connection() as conn:
+        conn.execute(query)
+
+
 
     # src/dagster_essentials/defs/assets/trips.py
 @dg.asset(
